6c24f34738d540d77048109c281ad2b0f343fc66
Денис Долгов <denis.dolgov@monopoly.su>
2023-07-21 13:57:55 +0700

    RestaurantCard component created

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import NavBar from "./components/NavBar";
 import AppHeader from "./components/AppHeader";
+import RestaurantCard from "./components/RestaurantCard";
 
 export default function Home() {
   return (
@@ -8,34 +9,9 @@ export default function Home() {
           <NavBar />
           <main>
             <AppHeader />
-            {/* CARDS */}
             <div className="py-3 px-36 mt-10 flex flex-wrap justify-center">
-              {/* CARD */}
-              <div
-                  className="w-64 h-72 m-3 rounded overflow-hidden border cursor-pointer"
-              >
-                <img
-                    src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
-                    alt=""
-                    className="w-full h-36"
-                />
-                <div className="p-1">
-                  <h3 className="font-bold text-2xl mb-2">Milestones Grill</h3>
-                  <div className="flex items-start">
-                    <div className="flex mb-2">*****</div>
-                    <p className="ml-2">77 reviews</p>
-                  </div>
-                  <div className="flex text-reg font-light capitalize">
-                    <p className=" mr-3">Mexican</p>
-                    <p className="mr-3">$$$$</p>
-                    <p>Toronto</p>
-                  </div>
-                  <p className="text-sm mt-1 font-bold">Booked 3 times today</p>
-                </div>
-              </div>
-              {/* CARD */}
+              <RestaurantCard />
             </div>
-            {/* CARDS */}
           </main>
         </main>
       </main>
