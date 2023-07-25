f8d433b1db9bca47bef06c87665c2a39b3c084fe
Денис Долгов <denis.dolgov@monopoly.su>
2023-07-25 18:13:26 +0700

    fetch restaurants by location

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,20 +1,47 @@
+import {Metadata} from "next";
+import {PrismaClient} from "@prisma/client";
 import Header from "./components/Header";
 import SearchSideBar from "./components/SearchSideBar";
 import RestaurantCard from "./components/RestaurantCard";
-import {Metadata} from "next";
+
+type Props = {
+    searchParams: {
+        city?: string;
+    }
+}
+
+const prisma = new PrismaClient();
 
 export const metadata: Metadata = {
     title: 'Search | Reservation App'
 }
 
-export default function Search() {
+const fetchRestaurantsByCity = async (city = '') => {
+    const restaurants = await prisma.restaurant.findMany({
+        where: {
+            location: {
+                name: {
+                    contains: city,
+                },
+            }
+        }
+    });
+
+    return restaurants;
+}
+
+export default async function Search({ searchParams: { city } }: Props) {
+    const restaurants = await fetchRestaurantsByCity(city);
+
     return (
         <>
-            <Header />
+            <Header/>
             <div className="flex py-4 m-auto w-2/3 justify-between items-start">
-                <SearchSideBar />
+                <SearchSideBar/>
                 <div className="w-5/6">
-                    <RestaurantCard />
+                    {restaurants.map(restaurant => (
+                        <RestaurantCard key={restaurant.id} restaurant={restaurant}/>
+                    ))}
                 </div>
             </div>
         </>
