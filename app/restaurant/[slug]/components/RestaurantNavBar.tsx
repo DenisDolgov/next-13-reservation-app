97ace56abd72b2292e1746e564f2cd426c402c62
Denis Dolgov <dolgovdo94@gmail.com>
2023-07-25 17:56:56 +0700

    restaurant page (#6)
    * restaurant detail page
    
    * restaurant menu page
    
    ---------
    
    Co-authored-by: Денис Долгов <denis.dolgov@monopoly.su>

--- app/restaurant/[slug]/components/RestaurantNavBar.tsx
+++ app/restaurant/[slug]/components/RestaurantNavBar.tsx
@@ -1,8 +1,13 @@
-export default function RestaurantNavBar() {
+import Link from "next/link";
+
+type Props = {
+    slug: string;
+};
+export default function RestaurantNavBar({ slug }:  Props) {
     return (
         <nav className="flex text-reg border-b pb-2">
-            <a href="" className="mr-7"> Overview </a>
-            <a href="" className="mr-7"> Menu </a>
+            <Link href={`/restaurant/${slug}`} className="mr-7"> Overview </Link>
+            <Link href={`/restaurant/${slug}/menu`} className="mr-7"> Menu </Link>
         </nav>
     )
 }
--- app/restaurant/[slug]/menu/page.tsx
+++ app/restaurant/[slug]/menu/page.tsx
@@ -1,16 +1,42 @@
+import {Metadata} from "next";
+import {PrismaClient} from "@prisma/client";
 import RestaurantNavBar from "../components/RestaurantNavBar";
 import Menu from "./components/Menu";
-import {Metadata} from "next";
+
+type Props = {
+    params: {
+        slug: string;
+    },
+};
+
+const prisma = new PrismaClient();
+
+const fetchRestaurantMenu = async (slug: string) => {
+    const restaurant = await prisma.restaurant.findUnique({
+        where: { slug },
+        select: {
+            items: true,
+        },
+    });
+
+    if (!restaurant) {
+        throw new Error();
+    }
+
+    return restaurant.items;
+};
 
 export const metadata: Metadata = {
     title: `Milesstone Grill's menu | Reservation App`
 };
 
-export default function RestaurantMenu() {
+export default async function RestaurantMenu({ params: { slug} }: Props) {
+    const menu = await fetchRestaurantMenu(slug);
+
     return (
         <div className="bg-white w-[100%] rounded p-3 shadow">
-            <RestaurantNavBar />
-            <Menu />
+            <RestaurantNavBar slug={slug} />
+            <Menu menu={menu} />
         </div>
     )
 }
