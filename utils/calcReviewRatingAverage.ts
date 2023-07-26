305ca9709bfa790c9a2ea8403806414178d3937b
Денис Долгов <denis.dolgov@monopoly.su>
2023-07-26 09:55:37 +0700

    util for calculate average rating created

---
+++ utils/calcReviewRatingAverage.ts
@@ -0,0 +1,9 @@
+import {Review} from "@prisma/client";
+
+export const calcReviewRatingAverage = (reviews: Review[]) => {
+    if (!reviews.length) return 0;
+
+    const sum = reviews.reduce((sum, item) => sum + item.rating, 0);
+
+    return Number((sum / reviews.length).toFixed(2));
+};
